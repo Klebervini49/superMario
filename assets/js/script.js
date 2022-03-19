d77091c496b832bc9e0f510922a8e5bb48b411ed
Kleberson <klebervini49@gmail.com>
2022-03-19 18:54:01 -0300

    add config name, vida, dificuldade

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,27 @@
  * Por favor, não me processa nintendo!!!!!!!!!!!
  */
 
-document.querySelector('#modelStart audio').play()
-
 /* Variaveis gerais*/
-let mscVolume = document.querySelector('#modelStart audio').volume = 0.03
-let mscIniciar = document.querySelectorAll('.pararIniciar')
-let mscTexto = document.querySelectorAll('.stopPlay')
+let mscVolume = (document.querySelector('#modelStart audio').volume = 0.03);
+let mscIniciar = document.querySelectorAll('.pararIniciar');
+let mscTexto = document.querySelectorAll('.stopPlay');
 
 let porcentagemCarregamentoInt = 0;
 
+//config
+function config() {
+    document.querySelector('.config').addEventListener('click', () => {
+        document.querySelector('.configInicio').style.display = 'flex';
+    });
+}
+config();
+
+function fecharModal() {
+    document.querySelector('.fechar').addEventListener('click', () => {
+        document.querySelector('.configInicio').style.display = 'none';
+    });
+}
+fecharModal();
 /* Tela de Start */
 
 /**
@@ -22,21 +34,25 @@ function startGame(e) {
     document.documentElement.requestFullscreen();
     setTimeout(() => {
         document.querySelector('#modelStart').style.display = 'none';
-    }, 200)
+    }, 200);
     setInterval(() => {
         if (porcentagemCarregamentoInt < 100) {
             porcentagemCarregamentoInt += 1;
-            document.querySelector('.progressBar').setAttribute('value', porcentagemCarregamentoInt)
-            document.querySelector('.totalProgress').innerText = `${porcentagemCarregamentoInt}%`
+            document
+                .querySelector('.progressBar')
+                .setAttribute('value', porcentagemCarregamentoInt);
+            document.querySelector(
+                '.totalProgress',
+            ).innerText = `${porcentagemCarregamentoInt}%`;
         }
         if (porcentagemCarregamentoInt >= 100) {
             setTimeout(() => {
                 document.querySelector('.loadingScreen').style.display = 'none';
-                document.querySelector('#modelStart audio').pause()
-            }, 2000)
+                document.querySelector('#modelStart audio').pause();
+            }, 2000);
         }
-    }, 30)
-};
+    }, 30);
+}
 
 /**
  * Entrando com Enter
@@ -46,55 +62,72 @@ function startGameEnter(e) {
         document.documentElement.requestFullscreen();
         setTimeout(() => {
             document.querySelector('#modelStart').style.display = 'none';
-        }, 200)
+        }, 200);
 
         setInterval(() => {
             if (porcentagemCarregamentoInt < 100) {
                 porcentagemCarregamentoInt += 1;
-                document.querySelector('.progressBar').setAttribute('value', porcentagemCarregamentoInt)
-                document.querySelector('.totalProgress').innerText = `${porcentagemCarregamentoInt}%`
+                document
+                    .querySelector('.progressBar')
+                    .setAttribute('value', porcentagemCarregamentoInt);
+                document.querySelector(
+                    '.totalProgress',
+                ).innerText = `${porcentagemCarregamentoInt}%`;
             } else {
                 document.querySelector('.loadingScreen').style.display = 'none';
-                document.querySelector('#modelStart audio').pause()
+                document.querySelector('#modelStart audio').pause();
             }
-        }, 30)
-    };
-};
+        }, 30);
+    }
+}
 
 /* MUSIC */
 
 function startStopMusic() {
-    if (document.querySelectorAll('.pararIniciar span')[0].classList.contains('parado') ||
-        document.querySelectorAll('.pararIniciar span')[1].classList.contains('parado')) {
-
-        document.querySelector('#modelStart audio').play()
+    if (
+        document
+            .querySelectorAll('.pararIniciar span')[0]
+            .classList.contains('parado') ||
+        document
+            .querySelectorAll('.pararIniciar span')[1]
+            .classList.contains('parado')
+    ) {
+        document.querySelector('#modelStart audio').play();
 
         mscIniciar.forEach((i1) => {
-            i1.querySelector('span').classList.remove('parado')
-            i1.querySelector('span').classList.add('ouvindo')
-        })
+            i1.querySelector('span').classList.remove('parado');
+            i1.querySelector('span').classList.add('ouvindo');
+        });
         mscTexto.forEach((i2) => {
-            i2.innerText = 'Pausar Música'
-        })
-
-    } else if (document.querySelectorAll('.pararIniciar span')[0].classList.contains('ouvindo') ||
-        document.querySelectorAll('.pararIniciar span')[1].classList.contains('ouvindo')) {
-
-        document.querySelector('#modelStart audio').pause()
+            i2.innerText = 'Pausar Música';
+        });
+    } else if (
+        document
+            .querySelectorAll('.pararIniciar span')[0]
+            .classList.contains('ouvindo') ||
+        document
+            .querySelectorAll('.pararIniciar span')[1]
+            .classList.contains('ouvindo')
+    ) {
+        document.querySelector('#modelStart audio').pause();
 
         mscIniciar.forEach((i3) => {
-            i3.querySelector('span').classList.add('parado')
-            i3.querySelector('span').classList.remove('ouvindo')
-        })
+            i3.querySelector('span').classList.add('parado');
+            i3.querySelector('span').classList.remove('ouvindo');
+        });
 
         mscTexto.forEach((i4) => {
-            i4.innerText = 'Iniciar Música'
-        })
+            i4.innerText = 'Iniciar Música';
+        });
     }
 }
 
-document.querySelectorAll('.pararIniciar')[0].addEventListener('click', startStopMusic)
-document.querySelectorAll('.pararIniciar')[1].addEventListener('click', startStopMusic)
+document
+    .querySelectorAll('.pararIniciar')[0]
+    .addEventListener('click', startStopMusic);
+document
+    .querySelectorAll('.pararIniciar')[1]
+    .addEventListener('click', startStopMusic);
 
 document.querySelector('#startButton').addEventListener('click', startGame);
 window.addEventListener('keydown', startGameEnter);
@@ -104,3 +137,49 @@ let todasImagens = document.querySelectorAll('img');
 todasImagens.forEach((item) => {
     item.setAttribute('draggable', false);
 });
+
+// nome vidas e dificuldade
+function defaultConfigs() {
+    if (!localStorage.configs) {
+        let configsInicio = {
+            nome_player: 'Mario',
+            vidas_player: 3,
+            dificuldade_player: 'Médio',
+        };
+
+        localStorage.configs = JSON.stringify(configsInicio);
+    }
+    infos = JSON.parse(localStorage.configs);
+    document.querySelector('.nome').value = infos.nome_player;
+    document.querySelector('.vidas').value = infos.vidas_player;
+    document.querySelector('.dificuldade').value = infos.dificuldade_player;
+}
+defaultConfigs();
+
+function trocarConfig() {
+    document.querySelector('button').addEventListener('click', (e) => {
+        e.preventDefault();
+        let nome = document.querySelector('.nome').value,
+            vidas = document.querySelector('.vidas').value,
+            dificuldade = document.querySelector('.dificuldade').value;
+
+        vidas = parseInt(vidas);
+        if (vidas > 3 || nome === '') {
+            alert(
+                'Nome não digitado ou número máximo de vidas ultrapassado(3)',
+            );
+        } else {
+            let configsInicio = {
+                nome_player: nome,
+                vidas_player: vidas,
+                dificuldade_player: dificuldade,
+            };
+
+            localStorage.configs = JSON.stringify(configsInicio);
+
+            infos = JSON.parse(localStorage.configs);
+            definirInfos();
+        }
+    });
+}
+trocarConfig();
