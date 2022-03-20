71364e85e820a45435dcc4b46b86d3b2890c82dd
Kleberson <klebervini49@gmail.com>
2022-03-20 00:18:01 -0300

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,141 +4,8 @@
  * Projeto sem fins lucrativos
  */
 
-/* Variaveis gerais*/
-let mscVolume = (document.querySelector('#modelStart audio').volume = 0.15);
-let mscIniciar = document.querySelectorAll('.pararIniciar');
-let mscTexto = document.querySelectorAll('.stopPlay');
-
-let porcentagemCarregamentoInt = 0;
-
-//config
-function config() {
-    document.querySelector('.config').addEventListener('click', () => {
-        document.querySelector('.configInicio').style.display = 'flex';
-    });
-}
-config();
-
-function fecharModal() {
-    document.querySelector('.fechar').addEventListener('click', () => {
-        document.querySelector('.configInicio').style.display = 'none';
-    });
-}
-fecharModal();
-/* Tela de Start */
-
-/**
- * Entrando com click
- */
-function startGame(e) {
-    document.documentElement.requestFullscreen();
-    setTimeout(() => {
-        document.querySelector('#modelStart').style.display = 'none';
-    }, 200);
-    setInterval(() => {
-        if (porcentagemCarregamentoInt < 100) {
-            porcentagemCarregamentoInt += 1;
-            document
-                .querySelector('.progressBar')
-                .setAttribute('value', porcentagemCarregamentoInt);
-            document.querySelector(
-                '.totalProgress',
-            ).innerText = `${porcentagemCarregamentoInt}%`;
-        }
-        if (porcentagemCarregamentoInt >= 100) {
-            setTimeout(() => {
-                document.querySelector('.loadingScreen').style.display = 'none';
-                document.querySelector('#modelStart audio').pause();
-            }, 2000);
-        }
-    }, 30);
-}
-
-/**
- * Entrando com Enter
- */
-function startGameEnter(e) {
-    if (e.key === 'Enter') {
-        document.documentElement.requestFullscreen();
-        setTimeout(() => {
-            document.querySelector('#modelStart').style.display = 'none';
-        }, 200);
-
-        setInterval(() => {
-            if (porcentagemCarregamentoInt < 100) {
-                porcentagemCarregamentoInt += 1;
-                document
-                    .querySelector('.progressBar')
-                    .setAttribute('value', porcentagemCarregamentoInt);
-                document.querySelector(
-                    '.totalProgress',
-                ).innerText = `${porcentagemCarregamentoInt}%`;
-            } else {
-                document.querySelector('.loadingScreen').style.display = 'none';
-                document.querySelector('#modelStart audio').pause();
-            }
-        }, 30);
-    }
-}
-
-/* MUSIC */
-
-function startStopMusic() {
-    if (
-        document
-            .querySelectorAll('.pararIniciar span')[0]
-            .classList.contains('parado') ||
-        document
-            .querySelectorAll('.pararIniciar span')[1]
-            .classList.contains('parado')
-    ) {
-        document.querySelector('#modelStart audio').play();
-
-        mscIniciar.forEach((i1) => {
-            i1.querySelector('span').classList.remove('parado');
-            i1.querySelector('span').classList.add('ouvindo');
-        });
-        mscTexto.forEach((i2) => {
-            i2.innerText = 'Pausar Música';
-        });
-    } else if (
-        document
-            .querySelectorAll('.pararIniciar span')[0]
-            .classList.contains('ouvindo') ||
-        document
-            .querySelectorAll('.pararIniciar span')[1]
-            .classList.contains('ouvindo')
-    ) {
-        document.querySelector('#modelStart audio').pause();
-
-        mscIniciar.forEach((i3) => {
-            i3.querySelector('span').classList.add('parado');
-            i3.querySelector('span').classList.remove('ouvindo');
-        });
-
-        mscTexto.forEach((i4) => {
-            i4.innerText = 'Iniciar Música';
-        });
-    }
-}
-
-document
-    .querySelectorAll('.pararIniciar')[0]
-    .addEventListener('click', startStopMusic);
-document
-    .querySelectorAll('.pararIniciar')[1]
-    .addEventListener('click', startStopMusic);
-
-document.querySelector('#startButton').addEventListener('click', startGame);
-window.addEventListener('keydown', startGameEnter);
-
-/* Game */
-let todasImagens = document.querySelectorAll('img');
-todasImagens.forEach((item) => {
-    item.setAttribute('draggable', false);
-});
-
 // nome vidas e dificuldade
+
 function defaultConfigs() {
     if (!localStorage.configs) {
         let configsInicio = {
@@ -146,16 +13,14 @@ function defaultConfigs() {
             vidas_player: 3,
             dificuldade_player: 'Médio',
         };
-
         localStorage.configs = JSON.stringify(configsInicio);
     }
     infos = JSON.parse(localStorage.configs);
     document.querySelector('.nome').value = infos.nome_player;
     document.querySelector('.vidas').value = infos.vidas_player;
     document.querySelector('.dificuldade').value = infos.dificuldade_player;
-    document.querySelector('.name_player').innerText = infos.nome_player;
 }
-defaultConfigs();
+defaultConfigs(); // CRIANDO AS INFORMAÇÕES PRIMARIAS
 
 function trocarConfig() {
     document.querySelector('button').addEventListener('click', (e) => {
@@ -179,11 +44,168 @@ function trocarConfig() {
             localStorage.configs = JSON.stringify(configsInicio);
 
             infos = JSON.parse(localStorage.configs);
-            document.querySelector('.name_player').innerText =
-                infos.nome_player;
-
+            console.log(infos);
             alert('Salvamento completo');
         }
     });
 }
-trocarConfig();
+trocarConfig(); // TROCANDO PARA AS INFORMAÇÕES ESCOLHIDAS PELO USUARIO
+
+/* Variaveis gerais*/
+let mscVolume = (document.querySelector('#modelStart audio').volume = 0.15);
+let porcentagemCarregamentoInt = 0;
+let coins = 0;
+
+//config
+function config() {
+    document.querySelector('.config').addEventListener('click', () => {
+        document.querySelector('.configInicio').style.display = 'flex'; //ABRINDO MODAL
+    });
+}
+config();
+function fecharModal() {
+    document.querySelector('.fechar').addEventListener('click', () => {
+        document.querySelector('.configInicio').style.display = 'none'; //FECHANDO MODAL
+    });
+}
+fecharModal();
+let inicioTimer;
+function tempoDeJogo() {
+    let hora = 00;
+    let minuto = 00;
+    let segundo = 00;
+
+    setInterval(() => {
+        let horaString = hora.toString();
+        if (horaString.length === 1) {
+            hora = `0${hora}`;
+        }
+        let minutoString = minuto.toString();
+        if (minutoString.length === 1) {
+            minuto = `0${minuto}`;
+        }
+        let segundoString = segundo.toString();
+        if (segundoString.length === 1) {
+            segundo = `0${segundo}`;
+        }
+
+        if (segundo < 59) {
+            ++segundo;
+        } else {
+            ++minuto;
+            segundo = 0;
+        }
+        if (minuto === 60) {
+            ++hora;
+            minuto = 0;
+        }
+        inicioTimer = `${hora}:${minuto}:${segundo}`;
+
+        document.querySelector('.time').innerText = `Timer: ${inicioTimer}`;
+    }, 1000);
+}
+
+/* TELA DE CARREGAMENTO */
+function startGame() {
+    document.documentElement.requestFullscreen();
+    setTimeout(() => {
+        let carregamentoTela = `
+        <!-- Tela de carregamento -->
+        <div class="loadingScreen">
+        
+            <h1 class="text-pop-up-top" style="text-align: center;"><span style="color: red;">S</span><span style="color: cyan;">u</span><span style="color: yellow;">p</span><span style="color: cyan;">e</span><span style="color: green;">r</span><br>
+                <span style="color: green;">M</span><span style="color: cyan;">a</span><span style="color: yellow;">r</span><span style="color: red;">i</span><span style="color: green;">o</span>
+                <span style="color: yellow;">W</span><span style="color: cyan;">o</span><span style="color: green;">r</span><span style="color: red;">l</span><span style="color: green;">d</span>
+            </h1>
+            <div>
+                <progress class="progressBar" value="0" max="100">10%</progress>
+                <span class="totalProgress">0%</span>
+            </div>
+        </div>`;
+
+        document.querySelector('body').innerHTML = carregamentoTela;
+    }, 100);
+
+    setInterval(() => {
+        if (porcentagemCarregamentoInt < 100) {
+            porcentagemCarregamentoInt += 1;
+            document
+                .querySelector('.progressBar')
+                .setAttribute('value', porcentagemCarregamentoInt);
+            document.querySelector(
+                '.totalProgress',
+            ).innerText = `${porcentagemCarregamentoInt}%`;
+
+            if (porcentagemCarregamentoInt >= 100) {
+                document.querySelector('.loadingScreen').remove();
+
+                let jogo = `<div class="game">
+                <header class="infos">
+                    <div class="lifes">
+                            ${infos.nome_player} x ${infos.vidas_player}
+                            <span class="amount"></span>
+                    </div>
+                    <div class="coins">Moedas: ${coins}</div>
+                    <div class="level">Level: 1</div>
+                    <div class="time"></div>
+                </header>
+                <main class="tela">
+                    <div class="cam">
+                        <div class="screen">
+                            <div class="players">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </main>
+                </div>`;
+                document.querySelector('body').innerHTML = jogo;
+                tempoDeJogo();
+
+                document.querySelector('#modelStart audio').pause();
+            }
+        }
+    }, 20);
+}
+function startGameEnter(e) {
+    if (e.key === 'Enter') {
+        startGame();
+    }
+}
+document.querySelector('#startButton').addEventListener('click', startGame);
+window.addEventListener('keydown', startGameEnter);
+
+/* MUSIC */
+function startStopMusic() {
+    if (
+        document
+            .querySelector('.pararIniciar span')
+            .classList.contains('parado')
+    ) {
+        document.querySelector('#modelStart audio').play();
+
+        document.querySelector('.pararIniciar').classList.remove('parado');
+        document.querySelector('.pararIniciar').classList.add('ouvindo');
+        document.querySelector('.stopPlay').innerText = 'Pausar Música';
+    } else if (
+        document
+            .querySelector('.pararIniciar span')
+            .classList.contains('ouvindo')
+    ) {
+        document.querySelector('#modelStart audio').pause();
+
+        document.querySelector('.pararIniciar').classList.remove('ouvindo');
+        document.querySelector('.pararIniciar').classList.add('parado');
+
+        document.querySelector('.stopPlay').innerText = 'Iniciar Música';
+    }
+}
+document
+    .querySelector('.pararIniciar')
+    .addEventListener('click', startStopMusic); // INICIANDO A FUNCÃO DE MÚSICA
+
+/* Game */
+let todasImagens = document.querySelectorAll('img');
+todasImagens.forEach((item) => {
+    item.setAttribute('draggable', false);
+}); // DEIXANDO TODAS AS IMAGEM SEM O ARRASTAR
